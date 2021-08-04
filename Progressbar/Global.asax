0178b7e4222d8c52d61f5dbc9989891802f2d062
Rahulbabu918 <72670279+Rahulbabu918@users.noreply.github.com>
2021-08-04 12:22:56 +0530

    Add project files.

---
+++ Progressbar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Progressbar.MvcApplication" Language="C#" %>
---
+++ Progressbar/Scripts/APIJavaScript.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+
+    $("#btnopen").click(function () {
+
+        $(".overlay,.popup").fadeIn();
+
+        ProgressBarStart();
+
+        APIcall();
+
+    });
+
+    function ProgressBarStart() {
+        var currentDate = new Date();
+        var second = currentDate.getSeconds();
+        if (second < 10) {
+            second = "0" + second;
+        }
+        $(".progress-bar").css('width', second + '%');            
+        setTimeout(function () {
+            ProgressBarStart()
+        }, 500);
+    }
+
+    function ProgressBarClose() {
+        $("#Fade_area").removeAttr("style");
+        $("myModal").removeAttr("style");
+
+    }
+
+    function APIcall() {
+        $.ajax({
+            url: "",
+            type: 'post',
+            ContentType: "application/json",
+            success: function () {
+                setTimeout(function () {
+                    ProgressBarClose()
+                }, 10000);
+            },
+            error: function () { 
+                setTimeout(function () {
+                    ProgressBarClose()
+                }, 10000);
+            }
+
+        });
+
+    }
+});
